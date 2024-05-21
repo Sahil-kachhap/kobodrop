dcf72ea0946b8268c338407cd92de39de324d168
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2024-05-22 02:26:26 +0530

    feat: complete features section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import Nav from './sections/Nav';
 import Hero from './sections/Hero';
+import Features from './sections/Features';
 
 function App() {
   return (
     <>
       <Nav />
       <Hero />
+      <Features />
     </>
   );
 }
 
 export default App;
+
