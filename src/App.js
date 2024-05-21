dc8ab390cb728329491f78dc63c5e9277d436cd0
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2024-05-21 19:12:20 +0530

    feat: complete hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Nav from './sections/Nav';
+import Hero from './sections/Hero';
 
 function App() {
   return (
     <>
       <Nav />
+      <Hero />
     </>
   );
 }
