f5479be9b824b8ea5bdc6ae50705c852bb31cb7c
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2024-05-25 02:39:40 +0530

    feat: complete footer section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from './sections/Hero';
 import Features from './sections/Features';
 import Partner from './sections/Partner';
 import Review from './sections/Review';
+import Footer from './sections/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Features />
       <Partner />
       <Review />
+      <Footer />
     </>
   );
 }
